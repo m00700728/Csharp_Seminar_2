5b5a9d21b9cf1c7dd9c671acd71d3347122ae1cd
Morozov <morozov7777777@mail.ru>
2023-12-26 00:58:25 +0300

    Task4_done

--- task4/Program.cs
+++ task4/Program.cs
@@ -1,1 +1,15 @@
 ﻿// Задача 4: Напишите программу, которая на вход принимает натуральное число N, а на выходе показывает его цифры через запятую.
+
+int number = new Random().Next(1, 100000);
+string str = Convert.ToString(number);
+int len = str.Length;
+char c;
+
+Console.Write($"Натуральное число {number} длиной {len} состоит из цифр: ");
+
+for (int i = 0; i < len - 1; i++) {
+    c = str[i];
+    Console.Write($"{c},");
+}
+c = str[len-1];
+Console.Write($"{c}");

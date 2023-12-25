7254e3f35fa6edfb688cc6f86f9dd95258cb9453
Morozov <morozov7777777@mail.ru>
2023-12-26 00:43:04 +0300

    Task3_done

--- task3/Program.cs
+++ task3/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 3: Напишите программу, которая принимает на вход целое число из отрезка [10, 99] и показывает наибольшую цифру числа.
+
+
+int number = new Random().Next(10, 100);
+
+string d1 = Convert.ToString(number);
+char s1 = d1[0];
+char s2 = d1[1];
+string compare = "";
+
+if (Convert.ToInt32(s1) > Convert.ToInt32(s2)) {
+    compare = "больше";
+} else if (Convert.ToInt32(s1) < Convert.ToInt32(s2)) {
+    compare = "меньше";
+} else if (Convert.ToInt32(s1) == Convert.ToInt32(s2)) {
+    compare = "равно(а)";
+}
+
+Console.WriteLine($"В числе {number} цифра {s1} {compare} {s2}");
+

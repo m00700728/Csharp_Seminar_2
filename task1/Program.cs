197fca6b1563e2e03a34a9bfc8dee0a9eea41a6d
Morozov <morozov7777777@mail.ru>
2023-12-25 22:01:30 +0300

    Task1 done

---
+++ task1/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 1: Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23.
+
+//int[] array = new int[new Random().Next(1,100)];
+int number = new Random().Next(1, 10000);
+// int number = 161;
+Console.Write($"Сгенерированное число {number} ");
+
+
+if (number % 7 == 0 && number % 23 == 0) {
+    Console.WriteLine("кратно 7 и 23");
+} else {
+    Console.WriteLine("некратно 7 и 23");
+}
